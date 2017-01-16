0e4f0aed2d34d133fe2185cb933dbd21a7d5cc47
MOHAMED ABDEL KADER <abdkadma@ubuntu-14.04.1-desktop-x8664-2disks-dev>
2017-01-16 18:49:56 +0300

    add Jeffs modifications/additions to color detection file

--- cvision/scripts/cvisionParams.py
+++ cvision/scripts/cvisionParams.py
@@ -23,17 +23,25 @@ def setParams():
     rospy.set_param('/getLaunchPad/liberal', False)              # allow lone bright white detection
     rospy.set_param('/getLaunchPad/reduction', 2)                # image dimension reduction for realtime processing speed
     rospy.set_param('/getLaunchPad/hoverLow', False)             # corner override (temp)
+    
     rospy.set_param('/getLaunchPad/imgShow', True)               # show processed images to screen
     rospy.set_param('/getLaunchPad/imgStream', True)             # stream reduced processed images
     rospy.set_param('/getLaunchPad/imgStreamRate', 3)            # streaming rate
     
-    # ROS parameters for color processing
-    rospy.set_param('/getColor/imgShow', True)                   # show processed images to screen
-
+    # ROS parameters getColors
+    rospy.set_param('/getColors/imgShow', True)                  # show processed images to screen
+    rospy.set_param('/getColors/imgStream', True)                # stream reduced processed images
+    rospy.set_param('/getColors/imgStreamRate', 3)               # streaming rate
+    rospy.set_param('/getColors/red','red')
+    rospy.set_param('/getColors/blue','blue')
+    rospy.set_param('/getColors/green','green')
+    rospy.set_param('/getColors/white','white')
     
     # ROS parameters for pix2m
     rospy.set_param('/pix2m/m2pix', 0.00104167)                  # 0.5m = 480pixels
-    rospy.set_param('/pix2m/altCal',1.2)                       # calibration altitude (used in main loop)
+    rospy.set_param('/pix2m/altCal',1.2)                         # calibration altitude (used in main loop)
+
+
 
 
  
