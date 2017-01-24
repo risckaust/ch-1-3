75d07b61bd16c8fda1dcb248876f887be42dc7cf
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2017-01-25 04:04:34 +1030

    change params

--- autopilots/scripts/autopilotParams.py
+++ autopilots/scripts/autopilotParams.py
@@ -14,20 +14,20 @@ def setParams():
     rospy.set_param('/autopilot/camOffset',0.0)      # camera offset from ground
 
     # ROS parameters for kAltVel
-    rospy.set_param('/kAltVel/gP',1.0)
+    rospy.set_param('/kAltVel/gP',2.0)
     rospy.set_param('/kAltVel/gI',0.1)
-    rospy.set_param('/kAltVel/vMaxU',1.0)
+    rospy.set_param('/kAltVel/vMaxU',2.0)
     rospy.set_param('/kAltVel/vMaxD',0.5)
 
     # ROS parameters for kBodVel
-    rospy.set_param('/kBodVel/gP',1.0)
+    rospy.set_param('/kBodVel/gP',1.5)
     rospy.set_param('/kBodVel/gI',0.1)
     rospy.set_param('/kBodVel/vMax',5.0)
     rospy.set_param('/kBodVel/gPyaw',0.5)           # yaw proportional gain
-    rospy.set_param('/kBodVel/yawOff',0.05)         # error to turn off yaw control (m)
+    rospy.set_param('/kBodVel/yawOff',1.0)         # error to turn off yaw control (m)
     rospy.set_param('/kBodVel/yawCone',45.0)        # cone to use proportional control (deg)
     rospy.set_param('/kBodVel/yawTurnRate',15.0)    # constant turn rate outside cone (deg/s)
-    rospy.set_param('/kBodVel/feedForward', True)   # use EKF and feedforward estimates
+    rospy.set_param('/kBodVel/feedForward', False)   # use EKF and feedforward estimates
 
 
 
