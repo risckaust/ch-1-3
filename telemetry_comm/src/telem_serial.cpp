a3faaecf8a367e220cd9fcdbfabf2b303bd86aac
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2017-02-28 17:00:09 +0300

    fix node name

--- telemetry_comm/src/telem_serial.cpp
+++ telemetry_comm/src/telem_serial.cpp
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
     * You must call one of the versions of ros::init() before using any other
     * part of the ROS system.
     */
-    ros::init(argc, argv, "gripper_node");
+    ros::init(argc, argv, "telem_serial");
 
     /**
     * NodeHandle is the main access point to communications with the ROS system.
