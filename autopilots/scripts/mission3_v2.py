66c14664d660bc2c06ed15b48991fdaaac6c3a40
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2017-08-15 23:08:50 +0300

    tighten position envelope

--- autopilots/scripts/mission3_v2.py
+++ autopilots/scripts/mission3_v2.py
@@ -1775,8 +1775,8 @@ def mission():
 	sm.SEARCH_ALT = 3.0
 	sm.PICK_ALT = 0.2
 	sm.DROP_ALT = 2.0
-	sm.ENVELOPE_XY_POS_MIN = 0.1
-	sm.ENVELOPE_XY_POS_MAX = 0.6
+	sm.ENVELOPE_XY_POS_MIN = 0.05
+	sm.ENVELOPE_XY_POS_MAX = 0.3
 	sm.ENVELOPE_XY_VEL_MIN = 0.15
 	sm.ENVELOPE_XY_VEL_MAX = 0.5
 	sm.vHold_factor = 0.05
